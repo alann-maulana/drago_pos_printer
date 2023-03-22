89f1464b3cfff102686192e70ac7a11bcc2f6e89
alann-maulana <alann_it06@yahoo.co.id>
2023-03-22 18:43:15 +0700

    some fixes

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import drago_blue_printer
 import drago_pos_printer
 import network_info_plus
+import path_provider_foundation
 import printing
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DragoBluePrinterPlugin.register(with: registry.registrar(forPlugin: "DragoBluePrinterPlugin"))
   DragoPosPrinterPlugin.register(with: registry.registrar(forPlugin: "DragoPosPrinterPlugin"))
   NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
 }
